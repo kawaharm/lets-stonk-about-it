20276ed1d28242921049a0d2f55b04e71d1413e2
kawaharm <mjbk295@gmail.com>
2022-01-27 10:53:04 -0800

    Updated navbar, started Gamestop page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // Imports
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
 
 // CSS
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Components
 import Tweet from './components/Tweet';
@@ -11,8 +14,27 @@ import Tweet from './components/Tweet';
 function App() {
   return (
     <Router>
+      <Navbar bg="navMain" variant="dark" sticky="top" expand="md" collapseOnSelect>
+        <Navbar.Brand>
+          <h4 style={{ marginLeft: "20px" }}>Let's Stonk About It</h4>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown title="Companies">
+              <NavDropdown.Item as={Link} to="/gme">GME</NavDropdown.Item>
+              <NavDropdown.Item href="">AAPL</NavDropdown.Item>
+              <NavDropdown.Item href="">TSLA</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="">About</Nav.Link>
+            <Nav.Link href="">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
       <Routes>
-        <Route path='/' element={<Tweet />} />
+        <Route path='/gme' element={<Tweet />} />
       </Routes>
     </Router>
   );
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,8 @@
 // Imports
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Button, Alert, Container, Row, Col, Stack } from 'react-bootstrap'
+
 
 // Use this key to connect to server
 const { REACT_APP_SERVER_URL } = process.env;
@@ -21,9 +23,39 @@ function Tweet(props) {
     }, []);
 
     return (
-        <div>
-            <h1>Let's {tweets} about it!</h1>
-        </div>
+        <Container fluid="xl">
+            <Stack>
+                <div className="bg-light border">First item</div>
+                <div className="bg-light border">Second item</div>
+                <div className="bg-light border">Third item</div>
+            </Stack>
+
+            <Row>
+
+                <Col className="p-5" style={{ backgroundColor: "gray" }}>
+                    <h1>Gamestop</h1>
+                    <h2>$GME</h2>
+                    <Row>
+                        <Col className="m-3 p-3" style={{ backgroundColor: "pink" }}>
+                            <div className="mb-2 text-center">Closing</div>
+                            <div className="p-2 border border-primary text-center">99.25</div>
+                        </Col>
+                        <Col className="m-3 p-3" style={{ backgroundColor: "purple" }}>
+                            <div className="mb-2 text-center">Sentiment Score</div>
+                            <div className="p-2 border border-primary text-center">99.25</div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="m-3" style={{ backgroundColor: "purple" }}>Day Low</Col>
+                        <Col className="m-3" style={{ backgroundColor: "pink" }}>Day High</Col>
+                    </Row>
+                </Col>
+
+                <Col style={{ backgroundColor: "green" }}>Tweets</Col>
+
+
+            </Row>
+        </Container >
     );
 }
 
