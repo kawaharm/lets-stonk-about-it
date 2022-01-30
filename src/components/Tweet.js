85fd0b543fe5b424a30c35d772aaacc90908d483
kawaharm <mjbk295@gmail.com>
2022-01-30 13:04:22 -0800

    updated Stock component to display closing price, day high/low

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,29 +2,81 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Button, Alert, Container, Row, Col, Stack, Image } from 'react-bootstrap'
+import { Button, Container, Row, Col, Image, Form } from 'react-bootstrap'
+import Stock from './Stock'
 
 
 // Use this key to connect to server
 const { REACT_APP_SERVER_URL } = process.env;
 
 
-function Tweet(props) {
+function Tweet() {
     // Hooks
     const [sentimentScore, setScore] = useState(0);
+    const [dateRange, setDateRange] = useState("")
+    const [stocks, setStocks] = useState([])
     const { id } = useParams();
 
+
     useEffect(() => {
-        const currentStock = { id }
+        const currentStock = { id };
         axios.post(`${REACT_APP_SERVER_URL}/tweets/`, currentStock.id)
             .then((response) => {
-                console.log(response.data)
                 setScore(response.data)
             })
             .catch((error) => {
                 console.log('ERROR: ', error);
             })
     }, []);
+
+    const handleChange = (e) => {
+        console.log('FROM SELECT before setDateRange: ', e.target.value)
+        console.log('dateRAnge before setDateRange: ', { dateRange })
+        setDateRange(e.target.value)
+        console.log('dateRange after setDateRange: ', { dateRange })
+    }
+
+    const handleSubmit = () => {
+        const date = new Date();
+        const today = date.toISOString().split('T')[0];
+        const dates = [];
+
+        if (dateRange === "1-day") {
+            dates.push(today, today);
+        } else if (dateRange === "5-day") {
+            let fiveDays = date.setDate(date.getDate() - 5)
+            fiveDays = new Date(fiveDays).toISOString().split('T')[0]
+            dates.push(fiveDays, today)
+        } else if (dateRange === "1-month") {
+            let oneMonth = date.setMonth(date.getMonth() - 1)
+            oneMonth = new Date(oneMonth).toISOString().split('T')[0]
+            dates.push(oneMonth, today)
+        }
+
+        const currentStock = { id }
+        axios
+            .post(`${REACT_APP_SERVER_URL}/stocks/`, {
+                ticker: currentStock.id,
+                dates: dates
+            })
+            .then((response) => {
+                console.log('RESPONSE FROM POLYGON', response.data);
+                setStocks(response.data)
+                console.log(stocks.results[0].c)
+
+            })
+            .catch((error) => {
+                console.log('ERROR: ', error);
+            });
+    }
+
+    const displayStocks = () => {
+        return <Stock
+            dayClose={stocks.results[0].c}
+            dayLow={stocks.results[0].h}
+            dayHigh={stocks.results[0].l}
+        />
+    }
 
     return (
         <Container fluid="xl">
@@ -34,23 +86,32 @@ function Tweet(props) {
                     <h2>$GME</h2>
                     <Row>
                         <Col className="m-3 p-3" style={{ backgroundColor: "pink" }}>
-                            <div className="mb-2 text-center">Closing</div>
-                            <div className="p-2 border border-primary text-center">99.25</div>
-                        </Col>
-                        <Col className="m-3 p-3" style={{ backgroundColor: "pink" }}>
-                            <div className="mb-2 text-center">Sentiment Score</div>
-                            <div className="p-2 border border-primary text-center"></div>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Control
+                                    as="select"
+                                    value={dateRange}
+                                    onChange={handleChange}
+                                >
+                                    <option>Choose</option>
+                                    <option value="1-day">1 Day</option>
+                                    <option value="5-day">5 Days</option>
+                                    <option value="1-month">1 Month</option>
+                                </Form.Control>
+                                <Button type="button" onClick={handleSubmit}>Submit form</Button>
+                            </Form>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col className="m-3" style={{ backgroundColor: "pink" }}>Day Low</Col>
-                        <Col className="m-3" style={{ backgroundColor: "pink" }}>Day High</Col>
-                    </Row>
+                    <Stock
+                        dayClose={stocks.results[0].c}
+                        dayLow={stocks.results[0].h}
+                        dayHigh={stocks.results[0].l}
+                    />
                 </Col>
                 <Col style={{ backgroundColor: "green" }}>
+                    <h1 className="mt-5 mb-3 text-center graphTitle">Average Sentiment Score for Tweets</h1>
                     <Image src={sentimentScore} />
                 </Col>
-            </Row>
+            </Row >
         </Container >
     );
 }
