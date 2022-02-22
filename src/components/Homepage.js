8a9ad3dfde81f4d60691bef019e034162a91300d
kawaharm <mjbk295@gmail.com>
2022-02-21 20:20:53 -0800

    updated homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,18 +6,14 @@ import { Button, Container, Row, Col, Image, Form } from 'react-bootstrap'
 function Homepage() {
     return (
         <div className="homepage">
-            <Container className="mt-5 container">
-                <h1 className="homeTitle text-white">Lets <span>Stonk</span></h1>
-                <h1 className="homeTitle text-white">About It</h1>
-                <h1 className="homeTitle">Lets Stonk About It</h1>
-                <h1 className="homeTitle">Lets Stonk About It</h1>
-                <h1 className="homeTitle">Lets Stonk About It</h1>
-                <h1 className="homeTitle">Lets Stonk About It</h1>
-                <h1 className="homeTitle">Lets Stonk About It</h1>
-                <h1 className="homeTitle">Lets Stonk About It</h1>
-
-            </Container>
-
+            <h1 className="homeTitle text-white">Lets <span>Stonk</span></h1>
+            <h3 className="homeSubTitle text-white">About It</h3>
+            <h1 className="homeTitle">Lets Stonk About It</h1>
+            <h1 className="homeTitle">Lets Stonk About It</h1>
+            <h1 className="homeTitle">Lets Stonk About It</h1>
+            <h1 className="homeTitle">Lets Stonk About It</h1>
+            <h1 className="homeTitle">Lets Stonk About It</h1>
+            <h1 className="homeTitle">Lets Stonk About It</h1>
         </div >
     )
 }
