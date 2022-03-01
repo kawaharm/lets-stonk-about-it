49a178d83ed643fdf4a40eba110c1d58d4dbef14
kawaharm <mjbk295@gmail.com>
2022-02-28 16:34:01 -0800

    updating homepage background

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,8 +5,8 @@ import { Button, Container, Row, Col, Image, Form } from 'react-bootstrap'
 
 function Homepage() {
     return (
-        <div className="homepage bg-dark">
-            <img className="homepage-image" src="./stonks_image.jpeg" alt="stonks meme image" />
+        <div className="homepage">
+            {/* <img className="homepage-image" src="./stonks_image.jpeg" alt="stonks meme image" /> */}
             <div className="homeTitle text-dark bg-light">
                 <p>Let's <span>Stonk</span></p>
                 <p className="homeSubTitle text-dark">About It</p>
