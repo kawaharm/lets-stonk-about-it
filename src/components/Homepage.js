e0e5572cce4919004259fe7186d7ea6e30962532
kawaharm <mjbk295@gmail.com>
2022-03-02 14:48:00 -0800

    styling tweetbox in homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,6 @@
 // Imports
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Card, Container, Row, Col, Image, Form } from 'react-bootstrap'
 
 
@@ -11,20 +12,62 @@ function Homepage() {
                 <p className="homeSubTitle text-dark">About It</p>
             </div>
             <div className="homeTweet text-dark bg-light">
-                <Avatar />
-                Tweet
+                <ProfilePic />
+                <div className="content">
+                    <NameAndHandle />
+                    <Message />
+                </div>
+            </div>
+
+            <div class="tweetBox text-dark bg-light">
+                <form>
+                    <div class="tweetContent">
+                        <img
+                            src="./stonks_image.jpeg"
+                            alt="profile pic"
+                        />
+                        <p>
+                            Ever thought about how posting messages on social media can impact the stock market?
+                            #Let's_Stonk_About_It is a web-based platform that examines the emotional elements
+                            of company-related tweets and compares it with its stock price in real-time.
+                        </p>
+                    </div>
+                    <Link to="/GME">
+                        <button class="tweetButton">Tweet</button>
+
+                    </Link>
+                </form>
             </div>
         </div >
     )
 }
 
-function Avatar() {
+function ProfilePic() {
     return (
         <img
             src="./stonks_image.jpeg"
             className="avatar"
-            alt="avatar"
+            alt="profile picture"
         />
+    )
+}
+
+function NameAndHandle() {
+    return (
+        <span className="nameAndHandle">
+            <strong className="name">Masaru</strong>
+            <span className="handle">@handle</span>
+        </span>
+    )
+}
+
+function Message() {
+    return (
+        <div className="message">
+            Ever thought about how posting messages on social media can impact the stock market?
+            Let's Stonk About It is a web-based platform that examines the emotional elements
+            of company-related tweets and compares it with its stock price in real-time.
+        </div>
     )
 }
 
