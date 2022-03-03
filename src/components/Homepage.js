0a2f32b229edffb5b82615126bc329fe53ba3116
kawaharm <mjbk295@gmail.com>
2022-03-02 16:28:41 -0800

    polished tweet on homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,6 @@
 // Imports
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Card, Container, Row, Col, Image, Form } from 'react-bootstrap'
 
 
 function Homepage() {
@@ -16,27 +15,8 @@ function Homepage() {
                 <div className="content">
                     <NameAndHandle />
                     <Message />
+                    <Buttons />
                 </div>
-            </div>
-
-            <div class="tweetBox text-dark bg-light">
-                <form>
-                    <div class="tweetContent">
-                        <img
-                            src="./stonks_image.jpeg"
-                            alt="profile pic"
-                        />
-                        <p>
-                            Ever thought about how posting messages on social media can impact the stock market?
-                            #Let's_Stonk_About_It is a web-based platform that examines the emotional elements
-                            of company-related tweets and compares it with its stock price in real-time.
-                        </p>
-                    </div>
-                    <Link to="/GME">
-                        <button class="tweetButton">Tweet</button>
-
-                    </Link>
-                </form>
             </div>
         </div >
     )
@@ -55,8 +35,8 @@ function ProfilePic() {
 function NameAndHandle() {
     return (
         <span className="nameAndHandle">
-            <strong className="name">Masaru</strong>
-            <span className="handle">@handle</span>
+            <strong className="name">StonkMaster</strong>
+            <span className="handle">@Lets_Stonk_About_It</span>
         </span>
     )
 }
@@ -65,8 +45,16 @@ function Message() {
     return (
         <div className="message">
             Ever thought about how posting messages on social media can impact the stock market?
-            Let's Stonk About It is a web-based platform that examines the emotional elements
+            <strong style={{ color: "#3366BB" }}>#Let's_Stonk_About_It</strong> is a web-based platform that examines the emotional elements
             of company-related tweets and compares it with its stock price in real-time.
+        </div>
+    )
+}
+
+function Buttons() {
+    return (
+        <div className="tweetButtons">
+            <button class="tweetButton">Tweet</button>
         </div>
     )
 }
